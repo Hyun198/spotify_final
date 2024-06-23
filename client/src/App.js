d8f980436f6126b069c2685c8f21bfa70be9bda6
Hyun <hwn123h@naver.com>
2024-06-23 21:38:34 +0900

    first

---
+++ client/src/App.js
@@ -0,0 +1,137 @@
+import { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import axios from 'axios';
+import React from 'react';
+import './App.css';
+
+function App() {
+  const [searchKey, setSearchKey] = useState("");
+  const [topArtist, setTopArtist] = useState(null);
+  const [topArtistTracks, setTopArtistTracks] = useState([]);
+  const [searchTracks, setSearchTracks] = useState([]);
+  const [selectedTrack, setSelectedTrack] = useState('');
+  const [lyrics, setLyrics] = useState(''); //원본 가사
+  const [translatedLyrics, setTranslatedLyrics] = useState(''); //번역된 가사
+  const [targetLang, setTargetLang] = useState('KO');
+  const [isTranslated, setIsTranslated] = useState(false) //번역되어있는지 flag
+
+
+  const searchArtists = async (e) => {
+    e.preventDefault();
+    const searchTerm = searchKey.trim();
+
+    if (!searchTerm) {
+      alert("검색어를 입력하세요.");
+      return;
+    }
+    try {
+      const response = await axios.post("/api/search", { searchTerm });
+      if (response.status !== 200) {
+        throw new Error("Failed to search artists");
+      }
+      const data = await response.data;
+      console.log(data);
+
+      setTopArtist(data.artist)
+      setTopArtistTracks(data.artistTracks);
+      setSearchTracks(data.searchTracks);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  const handleTrackSelect = async (track) => {
+    try {
+      console.log(track);
+      setSelectedTrack(track.name);
+      const response = await axios.post('/api/track/select', track);
+
+      if (response.status !== 200) {
+        throw new Error("Failed to send track");
+      }
+
+      const data = response.data;
+      const Lyrics = data.lyrics;
+
+      setLyrics(Lyrics);
+      setTranslatedLyrics('');
+      setIsTranslated(isTranslated) //false
+
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  const handleTranslate = async () => {
+    try {
+      const response = await axios.post('/api/translate', { lyrics, targetLang });
+      setTranslatedLyrics(response.data.text);
+      setIsTranslated(!isTranslated); //true
+      console.log('번역완료');
+
+    } catch (error) {
+      console.error('Error translating lyrics:', error.message);
+    }
+  }
+
+  const toggleTranslation = () => {
+    handleTranslate();
+    setIsTranslated(!isTranslated)
+  }
+
+
+  return (
+    <div className="App">
+      <h1 className='title'>Spotify Clone</h1>
+
+      <header className="header_container">
+        <form className="search_box" onSubmit={searchArtists} >
+          <box-icon name='search-alt'></box-icon>
+          <input className="search-input" type="text" placeholder='아티스트,노래' value={searchKey} onChange={(e) => setSearchKey(e.target.value)} />
+          <button className="search-btn" type="submit">검색</button>
+        </form>
+      </header>
+      <Container>
+        <Row>
+          <Col>
+            {topArtist && (
+              <div className="top-artist">
+                <img className="topArtist_imgs" src={topArtist.images[0].url} alt={topArtist.name} />
+                <p className="top-artist-name">{topArtist.name}</p>
+                <div className='top-artist-info'>
+                  {topArtist.genres.map(genre => (
+                    <p key={genre} className='genre'>{genre}</p>
+                  ))}
+                </div>
+              </div>
+            )}
+          </Col>
+          <Col>
+            <div className="top-artist-tracks">
+              {topArtistTracks.map((track) => (
+                <div key={track.id} className="top-artist-track" onClick={() => handleTrackSelect({ name: track.name, artist: track.artists[0].name })}>
+                  <img className="track-image" src={track.album.images[0].url} alt={track.album.name} />
+                  <div className="track-name">{track.name}</div>
+                </div>
+              ))}
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="lyrics_container">
+              <h2 className="selectedTrack-title">{selectedTrack}</h2>
+              <button className="translate-btn" onClick={toggleTranslation}>
+                {isTranslated ? '돌아가기' : '번역'}
+              </button>
+              <pre className='lyrics'>{isTranslated ? translatedLyrics : lyrics}</pre>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,173 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const qs = require('qs');
+const app = express();
+const cors = require('cors');
+
+const getLyrics = require('./lib/getLyrics');
+const getSong = require('./lib/getSong');
+const deepl = require('deepl-node')
+
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(cors());
+
+//Spotify api 설정
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const REDIRECT_URI = "http://localhost:3000"
+const AUTH_ENDPOINT = "https://accounts.spotify.com/api/token"
+const RESPONSE_TYPE = "token"
+
+
+const getAccessToken = async () => {
+    try {
+        const data = qs.stringify({
+            'grant_type': 'client_credentials',
+            'client_id': CLIENT_ID,
+            'client_secret': CLIENT_SECRET
+        });
+        const response = await axios.post(AUTH_ENDPOINT, data, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        return response.data.access_token;
+    } catch (error) {
+        console.error("Error getting access token:", error.message);
+        throw new Error("Failed to get access token");
+    }
+}
+const fetchArtist = async (token, searchTerm) => {
+    try {
+        const response = await axios.get("https://api.spotify.com/v1/search", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: searchTerm,
+                type: "artist",
+                market: "KR",
+                limit: 1
+            }
+        });
+        return response.data.artists.items[0]; // 첫 번째 가수 정보 반환
+    } catch (error) {
+        console.error('Error fetching artist:', error);
+        return null;
+    }
+};
+
+const fetchTrack = async (token, searchTerm) => {
+    try {
+        const response = await axios.get("https://api.spotify.com/v1/search", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            },
+            params: {
+                q: searchTerm,
+                type: "track",
+                market: "KR",
+                limit: 1
+            }
+        });
+        return response.data.tracks.items; // 노래 목록 반환
+    } catch (error) {
+        console.error('Error fetching track:', error);
+        return [];
+    }
+};
+
+
+app.post('/api/search', async (req, res) => {
+    const { searchTerm } = req.body;
+    try {
+        const token = await getAccessToken();
+        const [artist, tracks] = await Promise.all([
+            fetchArtist(token, searchTerm),
+            fetchTrack(token, searchTerm)
+        ]);
+
+        let topArtistData = null;
+        if (artist) {
+            const topArtistTracks = await axios.get(`https://api.spotify.com/v1/artists/${artist.id}/top-tracks`, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                },
+                params: {
+                    market: "KR"
+                }
+            });
+
+            topArtistData = {
+                artist,
+                tracks: topArtistTracks.data.tracks
+            };
+        }
+        // 검색어와 일치하는 트랙을 우선적으로 정렬
+        const sortedTracks = tracks.sort((a, b) => {
+            const aMatch = a.name.toLowerCase().includes(searchTerm.toLowerCase());
+            const bMatch = b.name.toLowerCase().includes(searchTerm.toLowerCase());
+            if (aMatch && !bMatch) return -1;
+            if (!aMatch && bMatch) return 1;
+            return 0;
+        });
+
+        res.json({
+            artist: topArtistData ? topArtistData.artist : null,
+            artistTracks: topArtistData ? topArtistData.tracks : [],
+            searchTracks: sortedTracks
+        });
+
+    } catch (error) {
+        console.error("Error searching artists:", error.message);
+        res.status(500).json({ error: "Failed to search artists" });
+    }
+});
+
+//track정보 받아오기
+app.post('/api/track/select', async (req, res) => {
+    try {
+        const { name, artist } = req.body;
+        console.log(name, artist);
+        const apiKey = process.env.GENIUS_API_KEY;
+        const options = {
+            apiKey: apiKey,
+            title: name,
+            artist: artist,
+            optimizeQuery: true,
+        };
+        const lyrics = await getLyrics(options)
+        console.log(lyrics);
+        console.log("가사 불러오기 성공");
+        res.json({ lyrics: lyrics });
+
+    } catch (error) {
+        console.error('error selected track:', error.message);
+        res.status(500).json({ error: 'failed to select track' });
+    }
+})
+
+app.post('/api/translate', async (req, res) => {
+    try {
+        const { lyrics, targetLang } = req.body;
+        console.log(lyrics, targetLang);
+        const apiKey = process.env.DEEPL_API_KEY;
+        const translator = new deepl.Translator(apiKey);
+        const result = await translator.translateText(lyrics, null, targetLang);
+        res.json(result);
+    } catch (error) {
+        console.error("Error translating lyrics:", error.message);
+        res.status(500).json({ error: "Failed to translate lyrics" });
+    }
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});

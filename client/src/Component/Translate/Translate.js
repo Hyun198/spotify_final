733af12b195c8c812fd0c9e05310df646660dcc1
Hyun <hwn123h@naver.com>
2024-09-18 13:37:05 +0900

    useref로 변경

--- client/src/Component/Translate/Translate.js
+++ client/src/Component/Translate/Translate.js
@@ -1,35 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import { Row, Col } from 'react-bootstrap';
+import React, { useEffect, useState, useRef } from 'react'
 import './Translate.style.css';
 
 const Translate = ({ lyrics, translatedLyrics, isTranslated, selectedTrack, toggleTranslation }) => {
     const [currentLyrics, setCurrentLyrics] = useState('');
-
+    const lyricsContainerRef = useRef(null);
     useEffect(() => {
-        const lyricsContainer = document.querySelector('.lyrics_container');
+
 
         setTimeout(() => {
-            lyricsContainer.classList.add('show');
-            setCurrentLyrics(isTranslated ? translatedLyrics : lyrics);
-        }, 100);
+            if (lyricsContainerRef.current) {
+                lyricsContainerRef.current.classList.add('show');
+                setCurrentLyrics(isTranslated ? translatedLyrics : lyrics);
+            }
+        }, 100); //가사에 약간의 애니메이션 부여
 
-        return () => {
-            lyricsContainer.classList.remove('show');
-        }
     }, [isTranslated, lyrics, translatedLyrics])
 
     return (
-        <Row>
-            <Col>
-                <div className="lyrics_container">
+        <div>
+            <div>
+                <div ref={lyricsContainerRef} className="lyrics_container">
                     <h2 className="selectedTrack-title">{selectedTrack.name}</h2>
                     <button className="translate-btn" onClick={toggleTranslation}>
                         {isTranslated ? '원문' : '번역'}
                     </button>
                     <pre className='lyrics'>{currentLyrics}</pre>
                 </div>
-            </Col>
-        </Row>
+            </div>
+        </div>
     )
 }
 
